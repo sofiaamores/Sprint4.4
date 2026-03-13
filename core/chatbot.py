cd6dfe225ae5087c0a98231ee42967c812c15a65
sofiaamores <147522531+sofiaamores@users.noreply.github.com>
2026-03-13 17:36:23 +0100

    Update chatbot.py

--- core/chatbot.py
+++ core/chatbot.py
@@ -13,22 +13,24 @@ load_dotenv()
 set_default_openai_key(os.getenv("OPENAI_API_KEY"))
 
 
+from agents import Runner
+from agents.main_agent import main_agent
+
+from .conversation import ConversationManager
+
+
 class Chatbot:
-    """
-    Lógica principal del chatbot.
-    """
 
     def __init__(self):
         self.conversation = ConversationManager()
 
-    def process_user_input(self, user_input: str):
+    def process_user_input(self, user_input):
 
         self.conversation.add_user_message(user_input)
 
-        result = Runner.run_sync(
-            main_agent,
-            user_input
-        )
+        print("Agente analizando la solicitud...")
+
+        result = Runner.run_sync(main_agent, user_input)
 
         response = result.final_output
 
