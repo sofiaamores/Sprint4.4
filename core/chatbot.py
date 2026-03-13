942b961f1a176a6e915474409f982f22d3b74914
sofiaamores <147522531+sofiaamores@users.noreply.github.com>
2026-03-13 17:29:16 +0100

    Update and rename chatbot.py to core/chatbot.py

---
+++ core/chatbot.py
@@ -0,0 +1,37 @@
+from agents import Runner
+from agents import set_default_openai_key
+
+import os
+from dotenv import load_dotenv
+
+from agents.main_agent import main_agent
+from .conversation import ConversationManager
+
+
+load_dotenv()
+
+set_default_openai_key(os.getenv("OPENAI_API_KEY"))
+
+
+class Chatbot:
+    """
+    Lógica principal del chatbot.
+    """
+
+    def __init__(self):
+        self.conversation = ConversationManager()
+
+    def process_user_input(self, user_input: str):
+
+        self.conversation.add_user_message(user_input)
+
+        result = Runner.run_sync(
+            main_agent,
+            user_input
+        )
+
+        response = result.final_output
+
+        self.conversation.add_assistant_message(response)
+
+        return response
